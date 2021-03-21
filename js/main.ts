ec67a3a6dd005b8392d76ac4125febfdfefd0035
Maciej <debniak@nonstop.co>
2021-03-21 15:49:54 +0100

    loading

--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,5 @@
 class StatsApp {
+  inputData: HTMLElement;
   data1Input: HTMLInputElement;
   data2Input: HTMLInputElement;
   data3Input: HTMLInputElement;
@@ -24,6 +25,7 @@ class StatsApp {
     this.data2Input = document.querySelector('#data2');
     this.data3Input = document.querySelector('#data3');
     this.data4Input = document.querySelector('#data4');
+    this.inputData  = document.querySelector('.input-data');
 
     this.sumInput  = document.querySelector('#sum');
     this.avgInput  = document.querySelector('#avg');
@@ -32,7 +34,7 @@ class StatsApp {
   }
 
   private watchInputValues() {
-    this.data1Input.addEventListener('input', () => this.computeData());
+    this.data1Input.addEventListener('input', () => this.generateInputs());
     this.data2Input.addEventListener('input', () => this.computeData());
     this.data3Input.addEventListener('input', () => this.computeData());
     this.data4Input.addEventListener('input', () => this.computeData());
@@ -44,6 +46,10 @@ class StatsApp {
     const data3 = +this.data3Input.value;
     const data4 = +this.data4Input.value;
 
+    if (!data1 || !data2 || !data3 || !data4) {
+      this.setLoadingIcon();
+    }
+
     const sum = data1 + data2 + data3 + data4;
     const avg = sum / 4;
     const min = Math.min(data1, data2, data3, data4);
@@ -51,13 +57,24 @@ class StatsApp {
 
     this.setData(sum, avg, min, max);
   }
-
+  private generateInputs() {
+    let amount: number = +this.data1Input.value;
+    for (let i: number = 0; i < amount; i++) {
+      let input: HTMLInputElement = document.createElement('input');
+      input.id = `data${i}`;
+      this.inputData.appendChild(input);
+    }
+  }
 
   private setData(sum: number, avg: number, min: number, max: number) {
     this.sumInput.value = String(sum);
     this.avgInput.value = String(avg);
     this.minInput.value = String(min);
     this.maxInput.value = String(max);
+  }
+
+  private setLoadingIcon() {
+    this.data1Input.style.background = "url('loading.svg')";
   }
 }
 const statsApp = new StatsApp();
